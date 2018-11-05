3dbca96f33be50114a86780af79e095db9b9deee
suhas023 <notsuhas@gmail.com>
2018-11-05 15:41:55 +0530

    Updates

---
+++ precache-manifest.c9e94e0a913b5de44dd14db0d9be4ce9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a799c072f192cfde29b",
+    "url": "/neighbourhood-maps/static/js/runtime~main.4a799c07.js"
+  },
+  {
+    "revision": "3408649ee31fd676c3f1",
+    "url": "/neighbourhood-maps/static/js/main.3408649e.chunk.js"
+  },
+  {
+    "revision": "6c08fbe3a4417af58da2",
+    "url": "/neighbourhood-maps/static/js/1.6c08fbe3.chunk.js"
+  },
+  {
+    "revision": "3408649ee31fd676c3f1",
+    "url": "/neighbourhood-maps/static/css/main.6f91c124.chunk.css"
+  },
+  {
+    "revision": "657bb796b39fb83b9ec6846aab143b3a",
+    "url": "/neighbourhood-maps/index.html"
+  }
+];
